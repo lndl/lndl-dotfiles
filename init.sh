cf01536eadda7c5348a9c512df8182765d408a45
Lautaro Nahuel De León <laudleon@gmail.com>
2016-03-24 21:28:57 -0300

    Some custom fixed added. Powerline will be installed only if Linux is present

--- init.sh
+++ init.sh
@@ -4,20 +4,23 @@
 echo "Configuring nvim..."
 ln -s "$HOME/lndl-dotfiles/nvim" ~/.config/nvim
 nvim -u "$HOME/lndl-dotfiles/nvim/plugins.vim" +PlugInstall +qall > /dev/null
-echo "alias vim=\"nvim\"" >> "$HOME/.zshrc"
 
 # Tmux
 echo "Configuring tmux..."
 ln -s "$HOME/lndl-dotfiles/tmux/tmux.conf" ~/.tmux.conf
-echo "alias tmux=\"TERM=screen-256color-bce tmux\"" >> "$HOME/.zshrc"
 
-# Powerline fonts
-echo "Cloning Powerline fonts repo"
-git clone \
-	https://github.com/powerline/fonts \
-	/tmp/powerline-fonts 2> /dev/null
+if [ $(uname) = "Darwin" ]
+then
+  echo "You're in OSX. Do nothing."
+else
+  # Powerline fonts
+  echo "Cloning Powerline fonts repo"
+  git clone \
+    https://github.com/powerline/fonts \
+    /tmp/powerline-fonts 2> /dev/null
 
-echo "Installing Powerline fonts repo..."
-/tmp/powerline-fonts/install.sh
+  echo "Installing Powerline fonts repo..."
+  /tmp/powerline-fonts/install.sh
+fi
 
 echo "All done"
