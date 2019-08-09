1456e8860b201e37bb49ed291c2739ee455b50ae
Lautaro Nahuel De León <laudleon@gmail.com>
2019-08-08 22:24:31 -0300

    rebind fzf preview command to ctrl-p and removed ccat support

--- zsh/zsh/fzf.zsh
+++ zsh/zsh/fzf.zsh
@@ -10,11 +10,7 @@ fzf-file-preview() {
   local cmd="${FZF_ALT_C_COMMAND:-"command find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
     -o -type f -print 2> /dev/null | sed 1d | cut -b3-"}"
   setopt localoptions noglobsubst pipefail 2> /dev/null
-  # cd "${$(eval "$cmd | $(__fzfcmd) --preview='head -$LINES {} | ccat -Calways'"):-.}"
- # eval "$cmd | fzf --preview='head -$LINES {} | ccat -Calways'" | while read item; do
- #   echo -n "${(q)item} "
- # done
-  echo -n "${$(eval "$cmd | fzf --preview='head -$LINES {} | ccat -Calways'")}"
+  echo -n "${$(eval "$cmd | fzf --preview='head -$LINES {} | cat'")}"
   local ret=$?
   zle redisplay
   typeset -f zle-line-init >/dev/null && zle zle-line-init
@@ -22,7 +18,7 @@ fzf-file-preview() {
 }
 
 zle     -N   fzf-file-preview
-bindkey '^J' fzf-file-preview
+bindkey '^P' fzf-file-preview
 
 fzf-git-select-files() {
   git status -s | fzf -m --border --header="HINT: (Un)Select files to commit with TAB. Move with CTRL+(j|k)" --preview="git --no-pager diff --color=always {2}" | cut -b '4-'
