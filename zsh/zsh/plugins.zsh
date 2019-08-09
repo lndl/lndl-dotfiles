ad1bb7b1fa8c1ba41b9c10265482c01a3dbd2404
Lautaro Nahuel De León <laudleon@gmail.com>
2019-08-09 12:36:10 -0300

    load zsh-substring-history for Macos

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -1,5 +1,5 @@
 if [ `uname` = "Darwin" ]; then
-  echo "Warning! There is no zsh plugins configuration for Macos. Review ~/.zsh/plugins.zsh in order to add some"
+  source "/usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
 else
   source "/usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh"
 fi
